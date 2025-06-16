d8240f59447e58f9109b895e3db8776d43186dd6
duyhungtnn <duyhung71089@gmail.com>
2025-06-16 21:40:53 +0700

    fix: lint fail

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { defineBKTConfig } from 'bkt-js-client-sdk';
 import type { BKTConfig, RawBKTConfig } from 'bkt-js-client-sdk';
-import { version } from 'react';
 import uuid from 'react-native-uuid';
+import { SDK_VERSION } from './version';
+import { SOURCE_ID_REACT_NATIVE } from './SourceId';
 
 export * from 'bkt-js-client-sdk';
-
-const SOURCE_ID_REACT_NATIVE = 10;
+export * from 'bkt-react-client-sdk';
 
 export function defineReactNativeBKTConfig(config: RawBKTConfig): BKTConfig {
   return defineBKTConfig({
@@ -14,7 +14,7 @@ export function defineReactNativeBKTConfig(config: RawBKTConfig): BKTConfig {
     // Set this to undefined to use the default in-memory storage
     storageFactory: undefined,
     wrapperSdkSourceId: SOURCE_ID_REACT_NATIVE,
-    wrapperSdkVersion: version,
+    wrapperSdkVersion: SDK_VERSION,
   });
 }
 
